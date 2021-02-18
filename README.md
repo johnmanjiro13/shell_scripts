7aaf84ddb888928a62e4fbbc3937e311bf1b82dc
johnmanjiro13 <juntama193@gmail.com>
2021-02-18 20:54:43 +0900

    install shell script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# scripts
+# shell_scripts
 collection of shell scripts
+
+# Install
+```
+curl https://raw.githubusercontent.com/johnmanjiro13/shell_scripts/master/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTS_PATH="$HOME"/dev/src/github.com/johnmanjiro13/shell_scripts/scripts; export SCRIPTS_PATH
+SCRIPTS_GITHUB="https://github.com/johnmanjiro13/shell_scripts.git"; export SCRIPTS_GITHUB
+
+download_scripts() {
+  if [ ! -d "$SCRIPTS_PATH" ]; then
+    git clone "$SCRIPTS_GITHUB"
+  fi
+}
+
+install() {
+  cd ./scripts
+  for script in *.sh
+  do
+    ln -sf "$SCRIPTS_PATH/$script" "/usr/local/bin/${script%.*}"
+  done
+}
+
+main() {
+  if [ -x "$(which git)" ] && [ -x "$(which curl)" ]; then
+    download_scripts && install
+  else
+    echo "Please install dependencies: ('git', 'curl')"
+    exit 1
+  fi
+}
+
+main
