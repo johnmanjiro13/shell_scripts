071794b00a09e6402997b3ea3ad926005976530b
johnmanjiro13 <juntama193@gmail.com>
2021-02-20 23:59:33 +0900

    check symbolic link in uninstall

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ collection of useful shell scripts
 ```
 curl https://raw.githubusercontent.com/johnmanjiro13/shell_scripts/master/install.sh | bash
 ```
+
+# Uninstall
+Run `uninstall.sh` script which is placed in shell_scripts directory.
+
+This directory is `$HOME/dev/src/github.com/johnmanjiro13/shell_scripts` by default.
--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+SCRIPTS_PATH="$HOME"/dev/src/github.com/johnmanjiro13/shell_scripts/scripts; readonly SCRIPTS_PATH
+
 uninstall() {
-  cd ./scripts
+  cd "$SCRIPTS_PATH"
   for script in *.sh; do
-    unlink "/usr/local/bin/${script%.*}"
+    link="/usr/local/bin/${script%.*}"
+    if [ -L "$link" ]; then
+      unlink "$link"
+    fi
   done
 }
 
